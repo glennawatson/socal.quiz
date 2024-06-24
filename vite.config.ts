77da29d089264c249ebd2ecb0090d33ba50da7e1
Glenn Watson <glenn@glennwatson.net>
2024-06-24 17:51:20 +1000

    Add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "vitest/config";
+import {defineConfig} from "vitest/config";
 
 export default defineConfig({
     test: {
         setupFiles: './tests/setupTests.ts',
         coverage: {
-          provider: 'v8',
-          exclude: [
-            '**/*.d.ts',
-            '**/*.interfaces.ts', // Exclude your interfaces file
-          ],
+            reporter: ['json'],
+            reportsDirectory: './coverage',
+            provider: 'v8',
+            exclude: [
+                '**/*.d.ts',
+                '**/*.interfaces.ts', // Exclude your interfaces file
+            ],
         },
-      },
+    },
 //   // ... other configurations ...
 //   test: {
 //     root: "./src", // Set the root directory for resolving imports in tests
